7fa27f09d1f5f79ac60e99fb08ef2406020af720
André Silva <andre.silva@posteo.de>
2025-01-09 15:12:33 +1000

    Add react-hook-form for better form handling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import RegistrationForm from "@/components/RegistrationForm";
-import Image from "next/image";
-
+import RegistrationFormWrapper from '@/components/RegistrationFormWrapper'
+import Image from 'next/image'
 export default function Home() {
   return (
-    <main
-      className="container mx-auto py-6 min-h-screen"
-      style={{ background: "linear-gradient(to top, #c5d556, #ffffff)" }}
-    >
+    <main className="container mx-auto py-6 min-h-screen" style={{ background: 'linear-gradient(to top, #c5d556, #ffffff)' }}>
       <div className="flex justify-start place-items-center">
         <Image
           src="/images/ADC_logo_no_bg.png"
@@ -16,8 +12,8 @@ export default function Home() {
         />
         <span className="text-8xl font-bold font-garda-empty ml-4">2025</span>
       </div>
-
-      <RegistrationForm />
+      <RegistrationFormWrapper />
     </main>
-  );
+  )
 }
+
