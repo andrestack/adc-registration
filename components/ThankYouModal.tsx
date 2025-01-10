40f7018bd7d8fc68c5bef259e96d179256305a44
André Silva <andre.silva@posteo.de>
2025-01-10 16:11:52 +1000

    Add thank you modal with email Link (pre email server)

---
+++ components/ThankYouModal.tsx
@@ -0,0 +1,52 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ThankYouModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function ThankYouModal({ isOpen, onClose }: ThankYouModalProps) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <Image src="/images/aldeia.jpg" alt="Logo" width={70} height={70} />
+
+            <DialogTitle className="text-2xl font-bold font-garda-empty">
+              Obrigado!
+            </DialogTitle>
+          </div>
+
+          <DialogDescription className="space-y-4 pt-3 text-black text-center">
+            <p>Sua inscrição foi submetida com sucesso.</p>
+            <p>
+              Por favor envie uma cópia do seu recibo de transferência bancária
+              para o nosso email
+            </p>
+            <div className="flex justify-center">
+              <Link
+                href="mailto:contact@aldeia-djembe-camp.com?subject=Comprovativo%20de%20Pagamento"
+                className="text-lg border-2 border-adc-green bg-adc-green rounded-md px-2 py-1 hover:bg-white"
+              >
+                Enviar
+              </Link>
+            </div>
+            <p>
+              Confirmaremos a sua inscrição assim que recebermos a confirmação
+              do pagamento.
+            </p>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
