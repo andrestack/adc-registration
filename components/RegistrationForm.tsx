d11a1033c9bfaae00dc4803b179edb5ce139227a
André Silva <124931972+andrestack@users.noreply.github.com>
2025-02-23 22:01:55 +1000

    Correct typo on download button

--- components/RegistrationForm.tsx
+++ components/RegistrationForm.tsx
@@ -229,7 +229,7 @@ export default function RegistrationForm() {
                   className="w-full"
                 >
                   <Download className="mr-2 h-4 w-4" />
-                  Download Receipt
+                  Download Recibo
                 </Button>
               </div>
 
